46fda92be46007349e93f7dc900349f241251658
Angus408 <mor0038@copperfieldcollege.vic.edu.au>
2025-06-23 13:34:15 +1000

    Practicing Basic Gui functions

--- gui.py
+++ gui.py
@@ -1,4 +1,13 @@
+'''Space for internal documentation of entire code purpose date started: 23/06/2025'''
+
 import customtkinter
 
 app = customtkinter.CTk()
+app.title("Stats2Drills GUI Interface")
+app.geometry("1250x700")
+
+button = customtkinter.CTkButton(app, text="my button")
+button.grid(row=2, column=4, padx=20, pady=20, sticky="ew", columnspan=2)
+
+
 app.mainloop()
