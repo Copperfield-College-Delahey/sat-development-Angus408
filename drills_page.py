f3848e51b86139269bd88365f0bcdf93ef0f5406
Angus408 <mor0038@copperfieldcollege.vic.edu.au>
2025-07-02 12:57:07 +1000

    Started to make pages in order to switch between

---
+++ drills_page.py
@@ -0,0 +1,54 @@
+import customtkinter as ctk
+from PIL import Image
+
+class Drills_page(ctk.CTkFrame):
+    def __init__(self, parent, controller=None):
+        super().__init__(parent)
+        #Drills Page content
+        #Configure main grid
+        self.grid_columnconfigure(0, weight=1) #Left column
+        self.grid_columnconfigure(1, weight=1) #Right column
+        self.grid_rowconfigure(0, weight=1)  #Top navigation bar
+        self.grid_rowconfigure(1, weight=1)  #Main page content
+
+        #Navigation bar
+        navigation_bar = ctk.CTkFrame(self, height=35, bg_color="#00A29E", fg_color="#00A29E")
+        navigation_bar.grid(row=0, column=0, columnspan=2, sticky="nsew")
+        navigation_bar.grid_columnconfigure(0, weight=1)
+        navigation_bar.grid_columnconfigure(1, weight=1)
+        navigation_bar.grid_columnconfigure(2, weight=1)
+        navigation_bar.grid_columnconfigure(3, weight=1)
+        navigation_bar.grid_columnconfigure(4, weight=1)
+        navigation_bar.grid_columnconfigure(5, weight=1)
+        navigation_bar.grid_columnconfigure(6, weight=1)
+        navigation_bar.grid_columnconfigure(7, weight=1)
+        navigation_bar.grid_columnconfigure(8, weight=1)
+        navigation_bar.grid_rowconfigure(0, weight=1)
+
+        logo = ctk.CTkImage(light_image=Image.open("Images/Stats Logo.png"), size=(75, 75))
+        logo_label = ctk.CTkLabel(navigation_bar, text="", image=logo)
+        logo_label.grid(row=0, column=0, sticky="nsew")
+
+        title_label = ctk.CTkLabel(navigation_bar, text="Stats2Drills", font=("ADLaM Display", 40), text_color="White")
+        title_label.grid(row=0, column=1, columnspan=2, sticky="nsew")
+
+        home_button = ctk.CTkButton(navigation_bar, width=120, height=40, text="Home", font=("ADLaM Display", 20), text_color="white", border_width=2, border_color="white", fg_color="#16CCCC", hover_color="#c7c7c7")
+        home_button.grid(row=0, column=3, sticky="e")
+        home_icon= ctk.CTkImage(light_image=Image.open("Images/white home icon.png"), dark_image=Image.open("Images/white home icon.png"), size=(35, 35))
+        home_icon_button = ctk.CTkButton(navigation_bar, text="", image=home_icon, bg_color="#00A29E", fg_color="#00A29E", height=30, width=35, hover_color="#c7c7c7")
+        home_icon_button.grid(row=0, column=4, sticky="w")
+        #Configure home_button to raise the relevant page
+        home_button.configure(command=lambda: show_frame("Home_page"))
+        home_icon_button.configure(command=lambda: show_frame("Home_page"))
+
+        drills_button = ctk.CTkButton(navigation_bar, height=40, text="Drills", font=("ADLaM Display", 20), text_color="white", border_width=2, border_color="white", fg_color="#16CCCC", hover_color="#c7c7c7")
+        drills_button.grid(row=0, column=5, sticky="e")
+        drills_icon= ctk.CTkImage(light_image=Image.open("Images/white drills icon.png"), dark_image=Image.open("Images/white drills icon.png"), size=(35, 35))
+        drills_icon_button = ctk.CTkButton(navigation_bar, text="", image=drills_icon, bg_color="#00A29E", fg_color="#00A29E",  height=30, width=35, hover_color="#c7c7c7")
+        drills_icon_button.grid(row=0, column=6, sticky="w")
+
+        previous_plans_button = ctk.CTkButton(navigation_bar, height=40, text="Previous Training Plans", font=("ADLaM Display", 20), text_color="white", border_width=2, border_color="white", fg_color="#16CCCC", hover_color="#c7c7c7")
+        previous_plans_button.grid(row=0, column=7, sticky="e")
+        file_icon= ctk.CTkImage(light_image=Image.open("Images/white file icon.png"), dark_image=Image.open("Images/white file icon.png"), size=(35, 35))
+        file_icon_button = ctk.CTkButton(navigation_bar, text="", image=file_icon, bg_color="#00A29E", fg_color="#00A29E",  height=30, width=35, hover_color="#c7c7c7")
+        file_icon_button.grid(row=0, column=8, sticky="w")
---
+++ main.py
@@ -0,0 +1,43 @@
+'''Stats2Drills GUI Application. date started: 23/06/2025'''
+
+import customtkinter as ctk
+from PIL import Image
+from home_page import Home_page
+from drills_page import Drills_page
+
+app = ctk.CTk(fg_color="#F2F2F2")
+app.title("Stats2Drills GUI Interface")
+app.geometry("1250x700")
+ 
+app.grid_columnconfigure(0, weight=1) #Single column
+app.grid_rowconfigure(0, weight=1)  #Single row 
+
+container = ctk.CTkFrame(app)
+container.grid(row=0, column=0, sticky="nsew", padx=5, pady=5)
+container.grid_rowconfigure(0, weight=1) # single row
+container.grid_columnconfigure(0, weight=1) # single column
+
+#Page switching functions
+
+# Load Home_page class
+home_page = Home_page(container)
+home_page.grid(row=0, column=0, sticky="nsew")
+
+#Load Drills_page class
+drills_page = Drills_page(container)
+drills_page.grid(row=0, column=0, sticky="nsew")
+
+#Dictionary of pages
+pages = {
+    "Home_page": home_page,
+    "Drills_page": drills_page
+}
+# Page-switching function
+def show_frame(page_name):
+    pages[page_name].tkraise()
+
+# Initially show Home Page
+show_frame("Home_page")
+
+
+app.mainloop()
