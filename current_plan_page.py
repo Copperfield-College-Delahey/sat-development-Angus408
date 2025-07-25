c1f338c6a89f714eb2b910d1ffe3a059630c47c5
Angus408 <mor0038@copperfieldcollege.vic.edu.au>
2025-07-25 17:42:50 +1000

    Worked on generate_plan function

--- current_plan_page.py
+++ current_plan_page.py
@@ -8,6 +8,8 @@ from drills import Drill_manager
 class Current_Plan_page(ctk.CTkFrame):
     def generate_plan(self, team_age, analysis):
         plan_contents = [["Time", "Drill", "Description/POE", "Diagram"]]
+        #Will keep track of drills added the plan
+        added_drill_keys = set()
 
         for row in analysis[1:]: #Skips header row
             system_drill_search = row[0]  #Focus
@@ -25,9 +27,12 @@ class Current_Plan_page(ctk.CTkFrame):
             if system_drill_search == "Freethrows":
                 for drill in self.drill_manager.drills:
                     if drill.drill_name == "Freethrows":
-                        freethrows = [drill.drill_duration, drill.drill_name, drill.drill_description, "N/A"]
-                        print("✅ Found Freethrows drill")
-                        break
+                        drill_key = (drill.drill_name, drill.drill_description)
+                        if drill_key not in added_drill_keys:
+                            freethrows = [drill.drill_duration, drill.drill_name, drill.drill_description, "N/A"]
+                            added_drill_keys.add(drill_key)
+                            print("✅ Found Freethrows drill")
+                            break
 
             for drill in self.drill_manager.drills:
                 #Checks for matching tag, will return true if at least one matches
@@ -35,34 +40,56 @@ class Current_Plan_page(ctk.CTkFrame):
                 if any(tag in drill.drill_tags for tag in tags):
                     #Checks if drill also matches team_age
                     if team_age in drill.drill_age:
-                        drill_details = [drill.drill_duration, drill.drill_name, drill.drill_description, drill.drill_diagram]
+                        #Creates key to check for duplicates
+                        drill_key = (drill.drill_name, drill.drill_description)
                         #Prevents same drill being added to list again
-                        if drill_details not in plan_contents:
-                            print(f"✅ Added drill: {drill.drill_name}")
+                        if drill_key not in added_drill_keys:
+                            if drill.drill_diagram != "N/A":   #Turns each drill diagram into a CtkImage allowing it to be placed into the table
+                                diagram = ctk.CTkImage(light_image=Image.open(drill.drill_diagram), dark_image=Image.open(drill.drill_diagram), size=(50, 50))
+                            else:
+                                diagram = "N/A"  #For Drills without a diagram ie. freethrows, they remain as N/A
+                            drill_details = [drill.drill_duration, drill.drill_name, drill.drill_description, diagram]
+                            print(f"Checking if drill already added: {drill.drill_name} (ID: {drill.drill_id})")
                             plan_contents.append(drill_details)
                             added_drills += 1
-
+                            added_drill_keys.add(drill_key)
                         #Stops getting drills for that focus after specific amount is reached
                         if added_drills >= amount:
                             break
+        total_time = 12 #starts at 12 to count offences and freethrows which is added last (after this runs)
+        
+        #Calulates total time for training plan
+        for row in plan_contents[1:]:
+            total_time += int(row[0])
+
+        #Most represnetative trainng sessions go for 1hr 30 min, so session must be at least 90 min
+        if total_time < 90:
+            for drill in self.drill_manager.drills:
+                if team_age in drill.drill_age:
+                        drill_key = (drill.drill_name, drill.drill_description)
+                        #Prevents same drill being added to list again
+                        if drill_key not in added_drill_keys:
+                            if drill.drill_diagram != "N/A":   #Turns each drill diagram into a CtkImage allowing it to be placed into the table
+                                diagram = ctk.CTkImage(light_image=Image.open(drill.drill_diagram), dark_image=Image.open(drill.drill_diagram), size=(50, 50))
+                            else:
+                                diagram = "N/A"  #For Drills without a diagram ie. freethrows, they remain as N/A
+                            drill_details = [drill.drill_duration, drill.drill_name, drill.drill_description, diagram]
+                            plan_contents.append(drill_details)
+                            added_drill_keys.add(drill_key)
+                            total_time += int(drill.drill_duration)
+                        #Prevents plan from going too far over timelimit.
+                        if 90 <= total_time < 100:
+                            break
+
         #Adds freethrows later in the plan. Players are most tired at this stage so best time to practice, as though in a game
         plan_contents.append(freethrows)
         #Offences always added last. Coaches practice whatever offences their team have, hence this is not a particular drill
         offences = [10, "Offences", "Work on your teams set offensive plays. Inlcuding your baseline and sideline out of bounds", "N/A"]
         plan_contents.append(offences)
 
-        total_time = 0
-        #Calulates total time for training plan
-        for row in plan_contents[1:]:
-            total_time += int(row[0])
-
-        if total_time < 90:
-            print("Error")
-
-
         #Table to display analysed stats
         generated_training_plan = CTkTable(master=self.plan_scrollable_frame, row=len(plan_contents), column=4, values=plan_contents, corner_radius=1, header_color="#b0b0b0", colors=["#f5f3f2", "#dedcdc"], border_width=1, border_color="#7a7878", font=("Abadi", 10), text_color="Black", wraplength=250, justify="left")
-        generated_training_plan.pack(expand=True)
+        generated_training_plan.pack(expand=True, fill="both")
 
         messagebox.showinfo("Success", "You have successfully generated a training plan")
 
@@ -76,17 +103,17 @@ class Current_Plan_page(ctk.CTkFrame):
         #Configure main grid
         self.grid_columnconfigure(0, weight=1) #Left column
         self.grid_columnconfigure(1, weight=1) #Right column
-        self.grid_rowconfigure(1, weight=1)  #Main page content
+        self.grid_rowconfigure(0, weight=1)  #Main page content
 
         #Right Column, area to display generated training plan
         self.right_column = ctk.CTkFrame(self, bg_color="#F2F2F2", fg_color="#F2F2F2")
         self.right_column.grid(row=0, column=1, sticky="nsew")
         self.right_column.grid_propagate(True)
         self.right_column.grid_columnconfigure(0, weight=1)
-        self.right_column.grid_rowconfigure(0, weight=1)
+        self.right_column.grid_rowconfigure(0, weight=0)
 
         #Scrollable frame to display generated plan
-        self.plan_scrollable_frame = ctk.CTkScrollableFrame(self.right_column, fg_color="#F2F2F2")
+        self.plan_scrollable_frame = ctk.CTkScrollableFrame(self.right_column, height=650, width=600, fg_color="#F2F2F2")
         self.plan_scrollable_frame.grid(row=0, column=0, sticky="nsew")
 
         #Left Column
@@ -136,7 +163,7 @@ class Current_Plan_page(ctk.CTkFrame):
         file_icon_button.configure(command=lambda: self.show_frame("Previous_plans_page"))
 
         save_icon = ctk.CTkImage(light_image=Image.open("Images/save icon.png"), dark_image=Image.open("Images/save icon.png"), size=(30, 30))
-        save_button = ctk.CTkButton(left_column, corner_radius=10, text="Save Training \nPlan", font=("ADLaM Display", 25), text_color="white", height=50, fg_color="#FF7A53", hover_color="#c7c7c7", image=save_icon, compound="right")
+        save_button = ctk.CTkButton(left_column, corner_radius=10, text="Download and Save", font=("ADLaM Display", 25), text_color="white", height=50, fg_color="#FF7A53", hover_color="#c7c7c7", image=save_icon, compound="right")
         save_button.grid(row=2, column=0, padx=10, pady=10, sticky="")
 
         #Row 1 - Left Column
