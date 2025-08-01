10b6cccd296d2a73bd3e7eb7603a2d737922c9b9
Angus408 <mor0038@copperfieldcollege.vic.edu.au>
2025-08-01 18:07:09 +1000

    Began the load and save for xml

--- drills.py
+++ drills.py
@@ -1,5 +1,7 @@
-#Class for drills to be based off
+import xml.etree.ElementTree as ET
+from tkinter import messagebox
 
+#Class for drills to be based off
 class Drill:
     def __init__(self, drill_id, drill_name, drill_tags, drill_age, drill_description, drill_duration, drill_diagram):
         self.drill_id = int(drill_id)  #drill_id can only be an integer
@@ -13,6 +15,48 @@ class Drill:
 #Class to manage all drills and related functions
 class Drill_manager:
     def __init__(self):
+
+        #Function to save drills onto xml file
+        def save_to_xml(self, filepath):
+            pass
+            root = ET.Element("drills")
+            for drill in self.drills:
+                drill_elem = ET.SubElement(root, "drill")
+
+                ET.SubElement(drill_elem, "drill_id").text = drill.drill_id
+                ET.SubElement(drill_elem, "drill_name").text = drill.drill_name
+                ET.SubElement(drill_elem, "drill_tags").text = ",".join(drill.drill_tags) #Returns list to just be string
+                ET.SubElement(drill_elem, "drill_age").text = drill.drill_age
+                ET.SubElement(drill_elem, "drill_description").text = drill.drill_description
+                ET.SubElement(drill_elem, "drill_duration").text = drill.drill_duration
+                ET.SubElement(drill_elem, "drill_diagram").text = drill.drill_diagram
+            
+            tree = ET.ElementTree(root)
+            tree.write(filepath, encoding="utf-8", xml_declaration=True)
+
+        #Function to load drills from xml file onto software
+        def load_from_xml(self, filepath):
+            pass
+            try:
+                tree = ET.parse(filepath)
+                root = tree.getroot()
+                #Loops through each drill element 
+                for drill_elem in root.findall("drill"):
+                    drill_id = drill_elem.find("drill_id").text
+                    drill_name = drill_elem.find("drill_name").text
+                    drill_tags = (drill_elem.find("drill_tags").text).split(",")
+                    drill_age = drill_elem.find("drill_age").text
+                    drill_description = drill_elem.find("drill_description").text
+                    drill_duration = drill_elem.find("drill_duration").text
+                    drill_diagram = drill_elem.find("drill_diagram").text
+                    #Create drill and add it to the list of drills
+                    drill = Drill(drill_id, drill_name, drill_tags, drill_age, drill_description, drill_duration, drill_diagram)
+                    self.drills.append(drill)
+            except FileNotFoundError:
+                messagebox.showerror("Error", "Drills xml file could not be found")
+
+
+
         d1 = Drill("0001", "Cincinnati Drill", "Shooting, Passing", "U8s, U10s, U12s, U14s, U16s, U18s, U20s", "1 passes to 2 who pivots and passes to 3 cutting to the basket for a shot or layup. 3 keeps going to the opposite outlet position, 2 rebounds, 1 replaces 2 and 2 outlets to 3. Encourage strong passing, cutting and finishing", "5", "Cincinnati")
         d2 = Drill("0002", "Star Drill", "Passing", "U8s, U10s, U12s, U14s, U16s, U18s, U20s", "One player is in each corner of the half court, or closer depending on team age/ability. Ball starts under the ring and is passed diagonally. The passer follows their pass moving to that spot. The drill continues with the ball being passed in the shape of a star, the final reciever completes a layup. Add jumpshots to extend drill", "5", "Star")
         d3 = Drill("0003", "3-Man Weave", "Passing, Offence", "U8s, U10s, U12s, U14s, U16s, U18s, U20s", "Create 3 lines on the baseline with the ball starting in the middle. The first player passes ahead before running behind that person, the reciever then passes to the other player and runs behind them. The general rule, whoever you pass to you run behind. Once the ball reaches half court the last person runs back to play defence creating a 2v1", "6", "3-Man weave")
